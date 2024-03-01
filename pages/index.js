3ae2999a7688d4e39f8cdc0643136c0061dccb96
Tawakaltu Mubarak <mubaraktawakaltu@gmail.com>
2024-03-01 17:40:48 +0100

    testing git branches

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,12 @@
 export default function Index() {
   return (
     <>
-    <p>Hello</p>
-    <p>world</p>
+    <main>
+      <div>
+        <h1> Learning about GIT branches</h1>
+      </div>
+      <p>hbfduuhcifh hreihireh  iheoihfier eruguregeru ugeyfqyeidg gvyyuwefgugefeu</p>
+    </main>
     </>
   );
 }
